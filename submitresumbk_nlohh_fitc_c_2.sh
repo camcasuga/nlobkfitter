9165ab8d87ffa1610762f33e35b31e1bd4acfbfd
carlisle_mahti <carlisle.doc.casuga@jyu.fi>
2024-08-26 11:24:39 +0300

    put in puhti

--- submitresumbk_nlohh_fitc_c_2.sh
+++ submitresumbk_nlohh_fitc_c_2.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 #### Argument order: SCHEME BK RC useImprovedZ2Bound useBoundLoop [Qs0 C^2 gamma] X0_if X0_bk e_c Q0sq Y0 eta0 old_sigma0 mass_charm mass_bottom | tee outputfile.dat
 #### ${1} = Qs0^2;  ${2} = C2;   ${3} = sigma0/2;  ${4} = gamma; ${5} = output file name
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/users/caaucasu/nlobkfitter/Minuit2/lib
 export LIBRARY_PATH=${LIBRARY_PATH}:/users/caaucasu/nlobkfitter/Minuit2/lib
-module add cmake/3.23.1
+module add cmake
 module add gsl
 
-OMP_NUM_THREADS=12 CUBACORES=0 ./build/bin/nlofit unlpc resumbk balsdrc z2imp unb ${1} ${2} ${4} 1.0 1.0 1 1 0 0 ${3} 1.42 4.83 | tee ./sigmars_nlo1/${5}.dat
+OMP_NUM_THREADS=4 CUBACORES=0 ./build/bin/nlofit uncc resumbk balsdrc z2imp unb ${1} ${2} ${4} 1.0 1.0 1 1 0 0 ${3} ${5} 4.83 | tee ./sigmars_nlo4/${6}.dat
 
